e5f307f15f72af49ceb5bf05bd27c5d80864868e
greggry <greggry.web@gmail.com>
2020-03-18 17:55:09 +0100

    Refactored script.js's code, added css variables in main.css.

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
-let appDiv = document.createElement('div'); // main window
+let app = document.createElement('div'); // main window
+app.className = 'app';
+document.body.appendChild(app);
+
 
 let title = document.createElement('h3');
-let questionDiv = document.createElement('div');
-
-let questionDivQuestion = document.createElement('h5');
-let questionDivAnswers = document.createElement('div');
-
-let answer1 = document.createElement('div');
-let answer2 = document.createElement('div');
-let answer3 = document.createElement('div');
-let answer4 = document.createElement('div');
-
-title.textContent = 'quiz';
-questionDivQuestion.textContent = 'dummy question?';
-answer1.textContent = 'answer1';
-answer2.textContent = 'answer2';
-answer3.textContent = 'answer3';
-answer4.textContent = 'answer4';
-
-
-appDiv.className = 'app';
 title.className = 'title';
-questionDiv.className = 'question';
-questionDivQuestion.className = 'questionDivQuestion';
-questionDivAnswers.className = 'questionDivAnswers';
-answer1.className = 'answer1';
-answer2.className = 'answer2';
-answer3.className = 'answer3';
-answer4.className = 'answer4';
+title.textContent = 'quiz';
+app.appendChild(title);
 
-document.body.appendChild(appDiv);
 
-appDiv.appendChild(title);
-appDiv.appendChild(questionDiv);
+let questionBlock = document.createElement('div');
+questionBlock.className = 'question-block';
+app.appendChild(questionBlock);
 
-questionDiv.appendChild(questionDivQuestion);
-questionDiv.appendChild(questionDivAnswers);
+let question = document.createElement('h5');
+question.className = 'question';
+question.textContent = 'dummy question?';
+questionBlock.appendChild(question);
 
-questionDivAnswers.appendChild(answer1);
-questionDivAnswers.appendChild(answer2);
-questionDivAnswers.appendChild(answer3);
-questionDivAnswers.appendChild(answer4);
+
+let answerBlock = document.createElement('div');
+answerBlock.className = 'answerBlock';
+questionBlock.appendChild(answerBlock);
+
+let answer = new Array(4);
+for (let i = 0; i < 4; i++) {
+  answer[i] = document.createElement('div');
+  answer[i].className = 'answer ' + i;
+  answer[i].textContent = 'answer ' + i;
+  answerBlock.appendChild(answer[i]);
+}
